901b0f961858e4d340534bb5c446203cc751e10f
Patrick Fuchs <patrick.fuchs@mailbox.org>
2023-03-09 19:03:31 +0100

    Compute centroids per class

--- instancesegmentation/loss/metricloss.py
+++ instancesegmentation/loss/metricloss.py
@@ -1,6 +1,25 @@
 import tensorflow as tf
 
 
+def compute_centroids(embeddings: tf.Tensor, labels: tf.Tensor) -> tf.Tensor:
+    """
+    Compute the centroids of each label.
+
+    Expects embeddings and labels in flattened form, i.e. (n, feature_count) and (n,). Emebddings
+    are expected to be floating point values, while the labels are integer values.
+    """
+    centroids = list()
+    number_of_instances = tf.math.reduce_max(labels)
+    for i in range(int(number_of_instances) + 1):
+        mask = labels == i
+        instance_size = tf.reduce_sum(tf.cast(mask, tf.float32))
+        if instance_size < 1e-6:
+            continue
+        centroid = tf.math.reduce_mean(embeddings[mask], axis=0)
+        centroids.append(centroid)
+    return tf.stack(centroids)
+
+
 class MetricLoss(tf.keras.layers.Layer):
     """
     Compute sample-wise push and pull losses.
