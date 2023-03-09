69c2e33d447486eb13dcb29730f768b64aa3e0bc
Patrick Fuchs <patrick.fuchs@mailbox.org>
2023-03-09 19:03:31 +0100

    Compute centroids per class

--- tests/loss/metricloss_test.py
+++ tests/loss/metricloss_test.py
@@ -1,4 +1,7 @@
-from instancesegmentation.loss.metricloss import MetricLoss
+import numpy as np
+import tensorflow as tf
+
+from instancesegmentation.loss.metricloss import compute_centroids, MetricLoss
 
 
 def test_creating_the_loss_layer() -> None:
@@ -9,3 +12,17 @@ def test_creating_the_loss_layer() -> None:
 def test_loss_layer_is_not_trainable() -> None:
     metric_loss = MetricLoss()
     assert metric_loss.trainable is False
+
+
+def test_compute_centroids() -> None:
+    embeddings = tf.constant(
+        [[1.0, 1.0], [0.0, 0.0], [0.2, 0.2], [0.1, 0.3], [0.3, 0.1], [0.4, 0.4]],
+        dtype=tf.float32,
+    )
+    labels = tf.constant([0, 0, 1, 1, 1, 2], tf.int32)
+    expected_centroids = np.array([[0.5, 0.5], [0.2, 0.2], [0.4, 0.4]], np.float32)
+
+    centroids = compute_centroids(embeddings, labels).numpy()
+
+    assert centroids.shape == (3, 2)
+    assert np.all(centroids == expected_centroids)
