Fix a corner case of loss computation if only one centroid is present

instancesegmentation/loss/metricloss.py
@@ -15,7 +15,7 @@ _default_error_measure = SquaredErrorMetric()
 
 
 def strictly_upper_triangular_matrix(x: tf.Tensor) -> tf.Tensor:
-    """Return the upper triangular matrix of a given matrix."""
+    """Return the strictly upper triangular matrix of a given matrix."""
     x = tf.linalg.band_part(x, 0, -1)
     x = tf.linalg.set_diag(x, tf.zeros((x.shape[0],)))
     return x
@@ -81,6 +81,8 @@ class MetricLoss(tf.keras.layers.Layer):
     def _compute_push_loss(self, centroids: tf.Tensor) -> tf.Tensor:
         """Push apart the different cluster centroids."""
         number_of_instances = tf.cast(tf.shape(centroids)[0], tf.float32)
+        if number_of_instances < 2:
+            return tf.constant(0.0)
         number_of_comparisons = number_of_instances * (number_of_instances - 1.0) / 2.0
         centroids = tf.tile(centroids[:, None, :], [1, centroids.shape[0], 1])
         distances = self.push_distance_metric(