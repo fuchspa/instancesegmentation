d898315acc0625f5f279f5acc61707f4e2b38c95
Patrick Fuchs <patrick.fuchs@mailbox.org>
2023-03-18 10:29:53 +0100

    Simplify scripts

--- scripts/inspect_circle_data.py
+++ scripts/inspect_circle_data.py
@@ -6,13 +6,7 @@ import numpy as np
 from skimage.color import label2rgb
 import typer
 
-from instancesegmentation.data.circledata import (
-    BackgroundProperties,
-    CircleProperties,
-    create_data_point,
-    Range,
-    Size,
-)
+from instancesegmentation.data.circledata import example_data_generator
 
 
 def main(export_path: Optional[Path] = None, seed: int = 1337) -> None:
@@ -23,23 +17,14 @@ def main(export_path: Optional[Path] = None, seed: int = 1337) -> None:
     export_path.mkdir(parents=True, exist_ok=True)
     np.random.seed(seed)
 
+    data_generator = example_data_generator()
+
     fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 15))
     for i in range(3):
-        circle_count = int(np.random.uniform(35, 60))
-        image, label = create_data_point(
-            Size(512, 512),
-            circle_count,
-            BackgroundProperties(gray_value=Range(4_000, 12_000)),
-            CircleProperties(radius=Range(40, 85), gray_value=Range(35_000, 42_000)),
-        )
-        axes[i][0].imshow(
-            image,
-            cmap="gray",
-            vmin=np.iinfo(image.dtype).min,
-            vmax=np.iinfo(image.dtype).max,
-        )
+        image, instance_label, _ = next(data_generator)
+        axes[i][0].imshow(image, cmap="gray", vmin=0.0, vmax=1.0)
         axes[i][0].set_axis_off()
-        axes[i][1].imshow(label2rgb(label, image))
+        axes[i][1].imshow(label2rgb(instance_label[..., 0], image[..., 0]))
         axes[i][1].set_axis_off()
 
     plt.savefig(export_path / "circle_data.png", dpi=600, bbox_inches="tight")
